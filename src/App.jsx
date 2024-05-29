b630d3ddc7d16fcc1a63df7a4b4f658e7d02755f
Lucas Alcocer <lucasalcocer174@gmail.com>
2024-05-29 18:50:08 -0300

    se ha llevado a cabo cambios importantes en el archivo App.jsx, de forma que finalmente es funcional con la funcion del react y la instalacion de axios

--- src/App.jsx
+++ src/App.jsx
@@ -6,22 +6,15 @@ export default class App extends Component {
   constructor(props) {
     super (props);
     this.state = {
+      categoria_id:"",
       categorias: []
     }
 
   }
 
-  render () {
-    return (
-    <div>
-      <span> App </span>
-      <input type="button" value="buscar" onClick={()=>this.buscarCategorias()}/>
-    </div>
-    )
-  }
-
-buscarCategorias () {
-  const url ="https://productos.ctpoba.edu.ar/api/categorias"
+  buscarCategorias () {
+  //const url ="https://productos.ctpoba.edu.ar/api/categorias"
+  const url ="http://10.0.4.103:3000/api/categorias"
   axios.get(url)
   .then((resp) => {
     this.setState ({categorias: resp.data.categorias})
@@ -30,15 +23,43 @@ buscarCategorias () {
     console.log(error);
   })
 }
+buscarProductos (categoria_id) {
+  const url ="http://10.0.4.103:3000/api/productos"
+  const config = {
+    params: {categoria_id}
+  }
+  axios.get (url, config)
+  .then((resp) => {
+    console.log(resp.data.productos);
+    this.setState ({productos: resp.data.productos})
+  })
+  .catch((error) => {
 
+  } )
 }
 
-<select
-  value = {this.state.categoria_id}
-    onCharge = {(e) => this.setState({cat.id: e.t.value})}
->
-  {this.state.categorias.map ((categoria,i)=> 
-  <option key={cat.id} value = {cat.id}> {cat.nombre} </option>
+render () {
+  return (
+    <div>
+      <span> App </span>
+      <input 
+      type="button" 
+      value="buscar cat" 
+      onClick={()=>this.buscarCategorias()}
+      />
+    <select
+      value = {this.state.categoria_id}
+      onCharge = {(e) => this.setState({categoria_id: e.target.value})}
+    >
+  {this.state.categorias.map ((cat,i)=> 
+  <option value={cat.id}> {cat.nombre} </option>
   )}
-</select> //mapear todo el atributo de categorias para crear opciones por cada vez que la API.. 
-
+  </select> 
+    <h3>{this.state.categoria_id}</h3>
+    {this.state.productos.map((prod, i) =>
+    <span key = {prod.id}> {prod.nombre} </span>
+  )}
+</div>
+  )
+}
+}
